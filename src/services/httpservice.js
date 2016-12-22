ec4bd91b2053377be0f2f1f45864f208a4b5a9bc
John Lander <john.lander@achillesinteractive.com>
2016-12-21 20:41:04 -0600

    Initialize repository

---
+++ src/services/httpservice.js
@@ -0,0 +1,25 @@
+var Fetch = require('whatwg-fetch');
+var baseURL = 'http://localhost:6069';
+
+var service = {
+    get: function(url) {
+        return fetch(baseURL + url)
+        .then(function(response) {
+            return response.json();
+        });
+    },
+    post: function(url, ingredient) {
+        return fetch(baseURL + url, {
+            headers: {
+                'Accept': 'text/plain',
+                'Content-Type': 'application/json'
+            },
+            method: 'post',
+            body: JSON.stringify(ingredient)
+        }).then(function(response) {
+            return response;
+        });
+    }
+};
+
+module.exports = service;
